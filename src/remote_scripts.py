d74a02e2def56afa367e56764aa0a430a35aa30e
michael.l@lightspin.io <michael.l@lightspin.io>
2021-03-19 00:40:04 +0200

    add lynis & chkrootkit reports

--- src/remote_scripts.py
+++ src/remote_scripts.py
@@ -2,10 +2,17 @@ script_a = '''#!/bin/bash -ex
 exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 
 apt-get update
-mkdir -p /home/ubuntu/vuls
-cd /home/ubuntu/vuls
-apt install docker.io -y 
+apt install docker.io build-essential binutils colorized-logs -y
 
+mkdir -p /home/ubuntu/vuls
+cd /home/ubuntu/
+wget https://downloads.cisofy.com/lynis/lynis-3.0.3.tar.gz
+wget ftp://ftp.pangeia.com.br/pub/seg/pac/chkrootkit.tar.gz
+mkdir -p chkrootkit && cd chkrootkit
+tar xvf /home/ubuntu/chkrootkit.tar.gz --strip-components 1
+make sense
+
+cd /home/ubuntu/vuls
 docker pull vuls/go-cve-dictionary
 docker pull vuls/goval-dictionary
 docker pull vuls/gost
@@ -134,6 +141,73 @@ sudo mount -o bind /sys /vol/sys
 sudo mount -o bind /run /vol/run
 
 sudo chroot /vol /bin/mount devpts /dev/pts -t devpts
+
+# Reporting
+mkdir -p /home/ubuntu/nginx/html
+cat > /home/ubuntu/nginx/default.conf <<EOF
+server {{
+    listen       80;
+    listen  [::]:80;
+    server_name  localhost;
+
+    #charset koi8-r;
+    #access_log  /var/log/nginx/host.access.log  main;
+    location /vuls/ {{
+	proxy_pass http://172.17.0.1:8000/;
+    }}
+    location / {{
+        root   /usr/share/nginx/html;
+        index  index.html index.htm;
+    }}
+  
+    #error_page  404              /404.html;
+
+    # redirect server error pages to the static page /50x.html
+    #
+    error_page   500 502 503 504  /50x.html;
+    location = /50x.html {{
+        root   /usr/share/nginx/html;
+    }}
+
+    # proxy the PHP scripts to Apache listening on 127.0.0.1:80
+    #
+    #location ~ \.php$ {{
+    #    proxy_pass   http://127.0.0.1;
+    #}}
+
+    # pass the PHP scripts to FastCGI server listening on 127.0.0.1:9000
+    #
+    #location ~ \.php$ {{
+    #    root           html;
+    #    fastcgi_pass   127.0.0.1:9000;
+    #    fastcgi_index  index.php;
+    #    fastcgi_param  SCRIPT_FILENAME  /scripts$fastcgi_script_name;
+    #    include        fastcgi_params;
+    #}}
+
+    # deny access to .htaccess files, if Apache's document root
+    # concurs with nginx's one
+    #
+    #location ~ /\.ht {{
+    #    deny  all;
+    #}}
+}}
+EOF
+
+sudo docker run --name docker-nginx -p {port}:80 -d -v /home/ubuntu/nginx/html:/usr/share/nginx/html -v /home/ubuntu/nginx/default.conf:/etc/nginx/conf.d/default.conf nginx
+
+# Lynis audit
+sudo cp /home/ubuntu/lynis-3.0.3.tar.gz /vol/root/
+sudo su -c "chroot /vol tar xvf /root/lynis-3.0.3.tar.gz -C /root/"
+sudo su -c "chroot /vol printf 'cd /root/lynis/\n./lynis audit system\n' > /vol/root/lynis/run.sh && chmod +x /vol/root/lynis/run.sh"
+sudo su -c "chroot /vol /root/lynis/run.sh" | ansi2html -l > /home/ubuntu/nginx/html/lynis_report.html
+
+# Chkrootkit scan
+cd /home/ubuntu/chkrootkit
+# sudo ./chkrootkit -r /vol | sed -n '/INFECTED/,/Searching/p' | head -n -1 | ansi2html -l > /home/ubuntu/nginx/html/chkrootkit_report.html
+sudo ./chkrootkit -r /vol | ansi2html -l > /home/ubuntu/nginx/html/chkrootkit_report.html
+
+# Vuls scan
 sudo su -c "chroot /vol /usr/sbin/sshd -p 2222 -o 'AuthorizedKeysFile=/root/.ssh/tmp_authorized_keys' -o 'AuthorizedKeysCommand=none' -o 'AuthorizedKeysCommandUser=none' -o 'GSSAPIAuthentication=no' -o 'UseDNS=no'"
 
 echo "Creating ssh config"
@@ -154,6 +228,7 @@ sudo docker run --rm -i \
 -v /etc/timezone:/etc/timezone:ro \
 vuls/vuls scan \
 -config=./config_scan.toml
+
 
 echo "Creating report..."
 sudo docker run --rm -i \
@@ -178,7 +253,7 @@ cd /home/ubuntu/vuls
 
 sudo docker run -dt --name vuls-report-srv-{instance_id} \
     -v $PWD:/vuls \
-    -p {port}:5111 \
+    -p 8000:5111 \
     ishidaco/vulsrepo
 
 echo "Check the report at: http://{ip_address}:{port}"
